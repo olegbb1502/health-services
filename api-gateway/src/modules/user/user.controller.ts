39fd1d8afbfcaf1bda01183bfaf91c7143fdf6b2
olegbb1502 <bolobanoleg@gmail.com>
2025-02-20 12:52:40 +0200

    Add guards based on auth service

--- api-gateway/src/modules/user/user.controller.ts
+++ api-gateway/src/modules/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Logger, Post, Body } from '@nestjs/common';
+import { Controller, Logger, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../../guards/auth.guard';
 
 import { UserService } from './user.service';
 import { User } from './dto';
 
 @Controller('user')
+@UseGuards(AuthGuard)
 export class UserController {
   private readonly logger = new Logger(UserController.name);
 
@@ -13,5 +15,11 @@ export class UserController {
   async createUser(@Body() user: User) {
     this.logger.log('Creating user');
     return this.userService.createUser(user);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
